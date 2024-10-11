665a7b6967734c6569b0a132f6d8d5f18c2fa2c9
olive-angelina <221fa04261@gmail.com>
2024-10-11 18:40:12 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const path = require('path');
-const cookieParser = require('cookie-parser'); // Needed to parse cookies
+const cookieParser = require('cookie-parser');
 
 const homeRoute = require('./routes/home');
 const authRoute = require('./routes/auth');
@@ -10,10 +10,10 @@ const dashboardRoute = require('./routes/dashboard');
 
 const app = express();
 
-// MongoDB Connection URL
+
 const dbUrl = "mongodb://localhost:27017/mydb";
 
-// Connect to MongoDB
+
 mongoose.connect(dbUrl, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
     console.log('Connected to MongoDB');
@@ -22,18 +22,18 @@ mongoose.connect(dbUrl, { useNewUrlParser: true, useUnifiedTopology: true })
     console.error('Error connecting to MongoDB:', error);
   });
 
-// Middleware
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser()); // Use cookie-parser middleware
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Routes
+
 app.use('/', homeRoute);
 app.use('/auth', authRoute);
 app.use('/dashboard', dashboardRoute);
 
-// Start the server
+
 const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
